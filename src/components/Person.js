e3ef4c179aab1913683b7946b0ae0be1544a9221
Faith <faithhafen@gmail.com>
2020-01-30 09:18:34 -0700

    tab name and icon updated

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Container, Segment, Header, Divider, Grid, Button } from 'semantic-ui-react'
+import { Container, Segment, Divider, Grid, Button } from 'semantic-ui-react'
 import axios from 'axios'
 
 class Person extends React.Component {
